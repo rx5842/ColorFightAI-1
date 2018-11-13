9e952743226ae38568a8a495bd0374d0e811f31c
rx5842 <ryan8xia@gmai.com>
2018-11-12 20:32:20 -0800

    pretty shitty imo

--- exampleAI.py
+++ exampleAI.py
@@ -1,32 +1,90 @@
 # You need to import colorfight for all the APIs
 import colorfight
 import random
+import math
 
 goldArr=[]
 engArr=[]
 myCells=[]
+target=(0,0)
 def goldEnergyArr():
 
     for x in range (g.width):
         for y in range(g.height):
             c=g.GetCell(x,y)
-            if c.cellType=="gold":
+            if c.cellType=="gold" and c.owner != g.uid:
                 goldArr.append((x,y))
-            if c.cellType=="energy":
+            if c.cellType=="energy" and c.owner != g.uid:
                 engArr.append((x,y))
 
 def attackMin():
     x=0
+
+def attackTarget(xt,yt):
+    myclose=(0,0)
+    cdist=10000
+    for cell in myCells:
+        x=cell[0]
+        y=cell[1]
+        if(distance(x,y,xt,yt)<cdist):
+            cdist=distance(x,y,xt,yt)
+            myclose=(x,y)
+
+    print("myclose",myclose)
+
+    if(xt<myclose[0]):
+        print(g.AttackCell(myclose[0]-1,myclose[1]))
+    if(xt>myclose[0]):
+        print(g.AttackCell(myclose[0]+1,myclose[1]))
+    if(yt<myclose[1]):
+        print(g.AttackCell(myclose[0],myclose[1]-1))
+    if(yt>myclose[1]):
+        print(g.AttackCell(myclose[0],myclose[1]+1))
+
+
 def getGold():
-    x=0
+    goldEnergyArr()
+    myclose=(0,0)
+    cdist=10000
+    for cell in myCells:
+        x=cell[0]
+        y=cell[1]
+        for gCell in goldArr:
+            gx=gCell[0]
+            gy=gCell[1]
+            if(distance(x,y,gx,gy)<cdist):
+                cdist=distance(x,y,gx,gy)
+                myclose=(x,y)
+                t=(gx,gy)
+    return t
+
+def distance(x1, y1, x2, y2):
+    return int((math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))))
+
+
 def getEnergy():
-    
+    goldEnergyArr()
+    myclose=(0,0)
+    cdist=10000
+    for cell in myCells:
+        x=cell[0]
+        y=cell[1]
+        for gCell in engArr:
+            gx=gCell[0]
+            gy=gCell[1]
+            if(distance(x,y,gx,gy)<cdist):
+                cdist=distance(x,y,gx,gy)
+                myclose=(x,y)
+                t=(gx,gy)
+    return t
 
 def getMyCells():
     for x in range(g.width):
         for y in range(g.height):
             if g.GetCell(x,y).owner==g.uid:
                 myCells.append((x,y))
+
+
 
 def base():
     if g.gold<60 or g.baseNum>2 or len(myCells)<25:
@@ -55,9 +113,18 @@ if __name__ == '__main__':
 
         while True:
             getMyCells()
-            base()
+            #base()
+            target=getGold()
+            print ("target",target)
+            attackTarget(target[0],target[1])
 
-            g.Refresh()
+
+        # while True:
+        #     getMyCells()
+        #     base()
+        #     getGold()
+        #
+        #     g.Refresh()
     else:
         print("Failed to join the game!")
 
